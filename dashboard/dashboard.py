b2acd9df8eaaf5916ac91d444d79f63b6258192b
agna2103 <161094297+agna2103@users.noreply.github.com>
2024-03-12 14:18:45 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,8 +6,8 @@ import numpy as np
 sns.set(style='dark')
 
 #membuka dataframe
-year_df = pd.read_csv("dashboard\years.csv")
-month_df = pd.read_csv("dashboard\month.csv")
+year_df = pd.read_csv("https://github.com/agna2103/l_analisis_data/blob/main/dashboard/years.csv")
+month_df = pd.read_csv("https://github.com/agna2103/l_analisis_data/blob/main/dashboard/month.csv")
 
 st.set_page_config(page_title= "AIR QUALITY INDEX SUBMISSION",page_icon="🌍",layout= "wide")
 
