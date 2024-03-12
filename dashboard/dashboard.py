a5141c3b293f97d41922a1211269e91a95ed011b
agna2103 <agna.aldhaka.210322@students.um.ac.id>
2024-03-12 11:22:31 +0700

    membuat file dashboard

---
+++ dashboard/dashboard.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+sns.set(style='dark')
+
+#membuka dataframe
+year_df = pd.read_csv("dashboard\years.csv")
+month_df = pd.read_csv("dashboard\month.csv")
+
+
+
